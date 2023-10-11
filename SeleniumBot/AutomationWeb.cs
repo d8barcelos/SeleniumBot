9e4b80943c4c4de77e98b08e56baf2096a019fde
Diogo <d8barcelos@gmail.com>
2023-10-11 10:12:00 -0300

    Initial commit

---
+++ SeleniumBot/AutomationWeb.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBot
+{
+    public class AutomationWeb
+    {
+        public IWebDriver driver;
+
+        public AutomationWeb()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public void TestWeb()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.ClassName("gLFyf")).SendKeys("Hello, World!");
+            driver.FindElement(By.ClassName("gNO89b")).Click();
+        }
+    }
+}
